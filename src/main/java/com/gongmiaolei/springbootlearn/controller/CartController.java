9c1aa3526b2cf8649e56135a3af7a0fda51e3641
CoderLei <CoderLei@qq.com>
2021-07-26 17:46:15 +0800

    second commit

--- src/main/java/com/gongmiaolei/springbootlearn/controller/CartController.java
+++ src/main/java/com/gongmiaolei/springbootlearn/controller/CartController.java
@@ -23,14 +23,14 @@ public class CartController {
 
     @GetMapping("/list")
     @ApiOperation("购物车列表")
-    public ApiRestResponse list(){
+    public ApiRestResponse list() {
         List<CartVO> cartList = cartService.list(UserFilter.currentUser.getId());
         return ApiRestResponse.success(cartList);
     }
 
     @PostMapping("/add")
     @ApiOperation("添加商品到购物车")
-    public ApiRestResponse add(@RequestParam Integer productId,@RequestParam Integer count){
+    public ApiRestResponse add(@RequestParam Integer productId, @RequestParam Integer count) {
         Integer id = UserFilter.currentUser.getId();
         List<CartVO> cartVOList = cartService.add(id, productId, count);
         return ApiRestResponse.success();
@@ -38,7 +38,7 @@ public class CartController {
 
     @PostMapping("/update")
     @ApiOperation("更新购物车")
-    public ApiRestResponse update(@RequestParam Integer productId,@RequestParam Integer count){
+    public ApiRestResponse update(@RequestParam Integer productId, @RequestParam Integer count) {
         Integer id = UserFilter.currentUser.getId();
         List<CartVO> cartVOList = cartService.update(id, productId, count);
         return ApiRestResponse.success();
@@ -46,7 +46,7 @@ public class CartController {
 
     @PostMapping("/delete")
     @ApiOperation("删除购物车")
-    public ApiRestResponse delete(@RequestParam Integer productId){
+    public ApiRestResponse delete(@RequestParam Integer productId) {
         Integer id = UserFilter.currentUser.getId();
         List<CartVO> cartVOList = cartService.delete(id, productId);
         return ApiRestResponse.success(cartVOList);
@@ -54,17 +54,17 @@ public class CartController {
 
     @PostMapping("/select")
     @ApiOperation("选择/不选择购物车的某商品")
-    public ApiRestResponse select(@RequestParam Integer productId,@RequestParam Integer selected){
+    public ApiRestResponse select(@RequestParam Integer productId, @RequestParam Integer selected) {
         Integer id = UserFilter.currentUser.getId();
-        List<CartVO> cartVOList = cartService.selectOrNot(UserFilter.currentUser.getId(),productId,selected);
+        List<CartVO> cartVOList = cartService.selectOrNot(UserFilter.currentUser.getId(), productId, selected);
         return ApiRestResponse.success(cartVOList);
     }
 
     @PostMapping("/selectAll")
     @ApiOperation("全选择/全不选择购物车的某商品")
-    public ApiRestResponse selectAll(@RequestParam Integer selected){
+    public ApiRestResponse selectAll(@RequestParam Integer selected) {
         Integer id = UserFilter.currentUser.getId();
-        List<CartVO> cartVOList = cartService.selectAllOrNot(id,selected);
+        List<CartVO> cartVOList = cartService.selectAllOrNot(id, selected);
         return ApiRestResponse.success(cartVOList);
     }
 }
